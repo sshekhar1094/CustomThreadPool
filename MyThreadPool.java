1ef5137bd98848c1ed7c9b21c1887410d6176c64
s0s01gz <shashank.shekhar@walmartlabs.com>
2019-02-08 23:38:07 +0530

    Calling run instead of start

--- MyThreadPool.java
+++ MyThreadPool.java
@@ -44,9 +44,12 @@ public class MyThreadPool {
 					task = queue.poll();			// get the task
 				}
 				
-				task.start();					// start
-				try { task.join();} 				// wait for this to finish before starting next task
-				catch (InterruptedException e) { e.printStackTrace(); }
+				// here calling run instead of start since start will create a new thread which we dont want
+				// we want the task to be executed inide this thread, 
+				// also calling run directly makes run act as a nomal method and will thus wait to finish, so no need to join
+				task.run();					// start
+//				try { task.join();} 				// wait for this to finish before starting next task
+//				catch (InterruptedException e) { e.printStackTrace(); }
 			}
 		}
 	}
